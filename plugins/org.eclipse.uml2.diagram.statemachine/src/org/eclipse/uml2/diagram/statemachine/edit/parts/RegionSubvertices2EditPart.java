667d6cc78afd247fd3eeada4fa612eebf6bd58f2
mgolubev <mgolubev>
2006-12-20 18:48:46 +0000

    [167840] - sgribovsky - Fixed endless loop due to to the wrong size calculations of collapse handles for regoin compartments

--- plugins/org.eclipse.uml2.diagram.statemachine/src/org/eclipse/uml2/diagram/statemachine/edit/parts/RegionSubvertices2EditPart.java
+++ plugins/org.eclipse.uml2.diagram.statemachine/src/org/eclipse/uml2/diagram/statemachine/edit/parts/RegionSubvertices2EditPart.java
@@ -1,12 +1,10 @@
 package org.eclipse.uml2.diagram.statemachine.edit.parts;
 
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.gef.EditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeCompartmentEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.CreationEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.DragDropEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
-import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ResizableCompartmentEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.figures.ResizableCompartmentFigure;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.ConstrainedToolbarLayout;
 import org.eclipse.gmf.runtime.notation.View;
@@ -51,7 +49,6 @@ public class RegionSubvertices2EditPart extends ShapeCompartmentEditPart {
 	 */
 	protected void createDefaultEditPolicies() {
 		super.createDefaultEditPolicies();
-		installEditPolicy(EditPolicy.PRIMARY_DRAG_ROLE, new ResizableCompartmentEditPolicy());
 		installEditPolicy(EditPolicyRoles.SEMANTIC_ROLE, new RegionSubvertices2ItemSemanticEditPolicy());
 		installEditPolicy(EditPolicyRoles.CREATION_ROLE, new CreationEditPolicy());
 		installEditPolicy(EditPolicyRoles.DRAG_DROP_ROLE, new DragDropEditPolicy());
--- plugins/org.eclipse.uml2.diagram.statemachine/src/org/eclipse/uml2/diagram/statemachine/view/factories/RegionSubverticesViewFactory.java
+++ plugins/org.eclipse.uml2.diagram.statemachine/src/org/eclipse/uml2/diagram/statemachine/view/factories/RegionSubverticesViewFactory.java
@@ -31,7 +31,6 @@ public class RegionSubverticesViewFactory extends ListCompartmentViewFactory {
 	 */
 	protected List createStyles(View view) {
 		List styles = new ArrayList();
-		styles.add(NotationFactory.eINSTANCE.createDrawerStyle());
 		styles.add(NotationFactory.eINSTANCE.createSortingStyle());
 		styles.add(NotationFactory.eINSTANCE.createFilteringStyle());
 		return styles;
