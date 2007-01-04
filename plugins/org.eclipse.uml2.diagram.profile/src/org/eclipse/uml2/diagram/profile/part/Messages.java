8bfd542d14768959b83682a1c68c9d1dd38ee126
mgolubev <mgolubev>
2007-01-04 11:31:35 +0000

    Generated i18n code is marked as @generated

--- plugins/org.eclipse.uml2.diagram.profile/src/org/eclipse/uml2/diagram/profile/part/Messages.java
+++ plugins/org.eclipse.uml2.diagram.profile/src/org/eclipse/uml2/diagram/profile/part/Messages.java
@@ -1,11 +1,20 @@
 package org.eclipse.uml2.diagram.profile.part;
 
+/**
+ * @generated
+ */
 public class Messages extends org.eclipse.osgi.util.NLS {
 
+	/**
+	 * @generated
+	 */
 	static {
 		org.eclipse.osgi.util.NLS.initializeMessages("messages", Messages.class); //$NON-NLS-1$
 	}
 
+	/**
+	 * @generated
+	 */
 	private Messages() {
 	}
 
