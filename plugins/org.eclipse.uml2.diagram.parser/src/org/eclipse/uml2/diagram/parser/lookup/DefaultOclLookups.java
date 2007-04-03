51300b681750da44453987efa0b4ff3e2ba1b9d2
mgolubev <mgolubev>
2007-04-03 12:52:55 +0000

    [180699] - Fixed: Property parser lookup failed with org.eclipse.emf.ocl.helper.OCLParsingException: ERROR in (operationCallExpCS): (Cannot find operation (union(Set(Type))) for the type (Set(T)))

--- plugins/org.eclipse.uml2.diagram.parser/src/org/eclipse/uml2/diagram/parser/lookup/DefaultOclLookups.java
+++ plugins/org.eclipse.uml2.diagram.parser/src/org/eclipse/uml2/diagram/parser/lookup/DefaultOclLookups.java
@@ -15,6 +15,7 @@ package org.eclipse.uml2.diagram.parser.lookup;
 public class DefaultOclLookups {
 
 	public static final String DEFAULT_TYPE_LOOKUP = "" + // 
+			" let result : Set(Type) = Set{} in " + //
 			" let pakkage : Package = self.getNearestPackage() in " + // 
 			" let siblings : Set(Type) = pakkage.ownedType in " + // 
 			" let imports : Bag(Type) = " + //
@@ -24,6 +25,6 @@ public class DefaultOclLookups {
 			" 		)->collect(" + //
 			"			importedElement.oclAsType(Type)" + //
 			"		) in " + // 
-			" Set{}->union(siblings)->union(imports->asSet()) ";
+			" result->union(siblings)->union(imports->asSet()) ";
 
 }
