17b087cfe77faf76d225bcab933a38d86e13cbb2
mgolubev <mgolubev>
2007-04-07 00:59:36 +0000

    [179301] -- Statemachine diagram tests -- getFile() method removed in GMF M6 is preserved via custom templates to make tests compiliable

--- plugins/org.eclipse.uml2.diagram.profile/src/org/eclipse/uml2/diagram/profile/part/UMLDiagramEditorUtil.java
+++ plugins/org.eclipse.uml2.diagram.profile/src/org/eclipse/uml2/diagram/profile/part/UMLDiagramEditorUtil.java
@@ -402,4 +402,18 @@ public class UMLDiagramEditorUtil {
 			return element2ViewMap;
 		}
 	} //LazyElement2ViewMap	
+
+	/**
+	 * @generated
+	 */
+	public static IFile getFile(org.eclipse.emf.common.util.URI uri) {
+		if (uri.toString().startsWith("platform:/resource")) { //$NON-NLS-1$
+			String path = uri.toString().substring("platform:/resource".length()); //$NON-NLS-1$
+			IResource workspaceResource = ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(path));
+			if (workspaceResource instanceof IFile) {
+				return (IFile) workspaceResource;
+			}
+		}
+		return null;
+	}
 }
