851a3e5cd35d744dadfc3fa9c1e8956c8ed2593e
mgolubev <mgolubev>
2007-05-22 17:14:06 +0000

    [164458] - GMF M7 - commands

---
+++ plugins/org.eclipse.uml2.diagram.statemachine/src/org/eclipse/uml2/diagram/statemachine/edit/commands/TransitionCreateCommand.java
@@ -0,0 +1,151 @@
+package org.eclipse.uml2.diagram.statemachine.edit.commands;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.emf.type.core.commands.CreateElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.CreateRelationshipRequest;
+import org.eclipse.uml2.diagram.statemachine.edit.policies.UMLBaseItemSemanticEditPolicy;
+import org.eclipse.uml2.uml.Region;
+import org.eclipse.uml2.uml.Transition;
+import org.eclipse.uml2.uml.UMLFactory;
+import org.eclipse.uml2.uml.UMLPackage;
+import org.eclipse.uml2.uml.Vertex;
+
+/**
+ * @generated
+ */
+public class TransitionCreateCommand extends CreateElementCommand {
+
+	/**
+	 * @generated
+	 */
+	private final EObject source;
+
+	/**
+	 * @generated
+	 */
+	private final EObject target;
+
+	/**
+	 * @generated
+	 */
+	private Region container;
+
+	/**
+	 * @generated
+	 */
+	public TransitionCreateCommand(CreateRelationshipRequest request, EObject source, EObject target) {
+		super(request);
+		this.source = source;
+		this.target = target;
+		if (request.getContainmentFeature() == null) {
+			setContainmentFeature(UMLPackage.eINSTANCE.getRegion_Transition());
+		}
+
+		// Find container element for the new link.
+		// Climb up by containment hierarchy starting from the source
+		// and return the first element that is instance of the container class.
+		for (EObject element = source; element != null; element = element.eContainer()) {
+			if (element instanceof Region) {
+				container = (Region) element;
+				super.setElementToEdit(container);
+				break;
+			}
+		}
+	}
+
+	/**
+	 * @generated
+	 */
+	public boolean canExecute() {
+		if (source == null && target == null) {
+			return false;
+		}
+		if (source != null && !(source instanceof Vertex)) {
+			return false;
+		}
+		if (target != null && !(target instanceof Vertex)) {
+			return false;
+		}
+		if (getSource() == null) {
+			return true; // link creation is in progress; source is not defined yet
+		}
+		// target may be null here but it's possible to check constraint
+		if (getContainer() == null) {
+			return false;
+		}
+		return UMLBaseItemSemanticEditPolicy.LinkConstraints.canCreateTransition_4001(getContainer(), getSource(), getTarget());
+	}
+
+	/**
+	 * @generated
+	 */
+	protected EObject doDefaultElementCreation() {
+		// org.eclipse.uml2.uml.Transition newElement = (org.eclipse.uml2.uml.Transition) super.doDefaultElementCreation();
+		Transition newElement = UMLFactory.eINSTANCE.createTransition();
+		getContainer().getTransitions().add(newElement);
+		newElement.setSource(getSource());
+		newElement.setTarget(getTarget());
+		return newElement;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected EClass getEClassToEdit() {
+		return UMLPackage.eINSTANCE.getRegion();
+	}
+
+	/**
+	 * @generated
+	 */
+	protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+		if (!canExecute()) {
+			throw new ExecutionException("Invalid arguments in create link command"); //$NON-NLS-1$
+		}
+		return super.doExecuteWithResult(monitor, info);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected ConfigureRequest createConfigureRequest() {
+		ConfigureRequest request = super.createConfigureRequest();
+		request.setParameter(CreateRelationshipRequest.SOURCE, getSource());
+		request.setParameter(CreateRelationshipRequest.TARGET, getTarget());
+		return request;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void setElementToEdit(EObject element) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Vertex getSource() {
+		return (Vertex) source;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Vertex getTarget() {
+		return (Vertex) target;
+	}
+
+	/**
+	 * @generated
+	 */
+	public Region getContainer() {
+		return container;
+	}
+}
