32949928333eb9de8a6d935b2b98c04280c5fe91
mgolubev <mgolubev>
2007-11-15 03:25:44 +0000

    [209644] - CenterLayout to use hinted preferred size of layouted figures

--- plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/draw2d/CenterLayout.java
+++ plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/draw2d/CenterLayout.java
@@ -30,11 +30,12 @@ public class CenterLayout extends StackLayout {
 		IFigure child;
 		for (int i = 0; i < children.size(); i++) {
 			child = (IFigure)children.get(i);
-			Dimension prefSize = child.getPreferredSize();
+			Dimension prefSize = child.getPreferredSize(r.width, r.height);
 			RECTANGLE.x = centerX - prefSize.width / 2;
 			RECTANGLE.y = centerY - prefSize.height / 2;
 			RECTANGLE.width = prefSize.width;
 			RECTANGLE.height = prefSize.height;
+			//Workaround for 209648
 			child.setBounds(RECTANGLE);
 		}
 	}
