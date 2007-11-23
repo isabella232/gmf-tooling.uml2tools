28217870f89b2017c9b96ed2fbd1602695a36008
mgolubev <mgolubev>
2007-11-23 05:46:31 +0000

    [204733] -- prefere rhomb over rectangle while selecting editpart for AssociationClass' member end

--- plugins/org.eclipse.uml2.diagram.clazz/src/org/eclipse/uml2/diagram/clazz/edit/policies/PackageCanonicalEditPolicy.java
+++ plugins/org.eclipse.uml2.diagram.clazz/src/org/eclipse/uml2/diagram/clazz/edit/policies/PackageCanonicalEditPolicy.java
@@ -698,6 +698,9 @@ public class PackageCanonicalEditPolicy extends CanonicalConnectionEditPolicy {
 		if (AssociationClassConnectorEditPart.VISUAL_ID == descriptor.getVisualID()) {
 			return getHintedEditPart(descriptor.getSource(), domain2NotationMap, AssociationClass2EditPart.VISUAL_ID);
 		}
+		if (Property7EditPart.VISUAL_ID == descriptor.getVisualID()) {
+			return getHintedEditPart(descriptor.getSource(), domain2NotationMap, AssociationClassRhombEditPart.VISUAL_ID);
+		}
 		return getSourceEditPartGen(descriptor, domain2NotationMap);
 	}
 
@@ -713,9 +716,6 @@ public class PackageCanonicalEditPolicy extends CanonicalConnectionEditPolicy {
 	 */
 	private EditPart getTargetEditPart(UMLLinkDescriptor descriptor, Domain2Notation domain2NotationMap) {
 		if (AssociationClassConnectorEditPart.VISUAL_ID == descriptor.getVisualID()) {
-			return getHintedEditPart(descriptor.getDestination(), domain2NotationMap, AssociationClassRhombEditPart.VISUAL_ID);
-		}
-		if (Property7EditPart.VISUAL_ID == descriptor.getVisualID()) {
 			return getHintedEditPart(descriptor.getDestination(), domain2NotationMap, AssociationClassRhombEditPart.VISUAL_ID);
 		}
 		return getTargetEditPartGen(descriptor, domain2NotationMap);
