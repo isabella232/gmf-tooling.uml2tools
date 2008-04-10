49520acbdc624a03d810deefe88674ad8dbf7826
tfesenko <tfesenko>
2008-04-10 09:07:34 +0000

    Generated test suite for Node tool group

--- tests/org.eclipse.uml2.diagram.clazz.tests/src/org/eclipse/uml2/diagram/clazz/tests/UITests.java
+++ tests/org.eclipse.uml2.diagram.clazz.tests/src/org/eclipse/uml2/diagram/clazz/tests/UITests.java
@@ -16,6 +16,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.eclipse.uml2.diagram.clazz.tests.action.ChangeInterfaceNotationActionTest;
+import org.eclipse.uml2.diagram.clazz.tests.tool.PaletteNodesGroupTest;
 import org.eclipse.uml2.diagram.clazz.tests.wholediagram.TestWholeDiagramSuite;
 
 public class UITests {
@@ -25,6 +26,7 @@ public class UITests {
 		//$JUnit-BEGIN$
 		suite.addTest(new TestWholeDiagramSuite());
 		suite.addTestSuite(ChangeInterfaceNotationActionTest.class);
+		suite.addTestSuite(PaletteNodesGroupTest.class);
 		//$JUnit-END$
 		return suite;
 	}
