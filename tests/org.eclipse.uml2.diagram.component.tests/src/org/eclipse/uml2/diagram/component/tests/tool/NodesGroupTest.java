6cca367e4d9e01c8bc12bc55ba4f3134e5ebe901
tfesenko <tfesenko>
2008-04-14 19:02:25 +0000

    Generated tests for 'Nodes' tool group - more testcases

--- tests/org.eclipse.uml2.diagram.component.tests/src/org/eclipse/uml2/diagram/component/tests/tool/NodesGroupTest.java
+++ tests/org.eclipse.uml2.diagram.component.tests/src/org/eclipse/uml2/diagram/component/tests/tool/NodesGroupTest.java
@@ -10,11 +10,23 @@ import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.tools.UnspecifiedTypeCreationTool;
 import org.eclipse.gmf.runtime.emf.type.core.ElementTypeRegistry;
 import org.eclipse.uml2.diagram.component.edit.parts.Artifact2EditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.ArtifactEditPart;
 import org.eclipse.uml2.diagram.component.edit.parts.Class2EditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.Class3EditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.ClassClassesEditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.ClassDiagramNotationClassEditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.ClassDiagramNotationInnerClassEditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.ClassEditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.Component2EditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.Component3EditPart;
 import org.eclipse.uml2.diagram.component.edit.parts.ComponentContentsEditPart;
 import org.eclipse.uml2.diagram.component.edit.parts.ComponentEditPart;
 import org.eclipse.uml2.diagram.component.edit.parts.Interface2EditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.InterfaceEditPart;
 import org.eclipse.uml2.diagram.component.edit.parts.Package3EditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.Package4EditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.PackageClassifiersEditPart;
+import org.eclipse.uml2.diagram.component.edit.parts.PackagePackagesEditPart;
 import org.eclipse.uml2.diagram.component.edit.parts.PropertyEditPart;
 import org.eclipse.uml2.diagram.component.part.UMLPaletteFactory;
 import org.eclipse.uml2.diagram.component.part.UMLVisualIDRegistry;
@@ -59,6 +71,7 @@ public class NodesGroupTest extends ComponentDCreationToolTestBase {
 	/**
 	 * @generated
 	 */
+	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		myRoot = new PaletteRoot();
@@ -74,6 +87,7 @@ public class NodesGroupTest extends ComponentDCreationToolTestBase {
 	/**
 	 * @generated
 	 */
+	@Override
 	protected void tearDown() throws Exception {
 		myRoot = null;
 		myComponentTool = null;
@@ -192,6 +206,230 @@ public class NodesGroupTest extends ComponentDCreationToolTestBase {
 		IGraphicalEditPart editPart7 = diagramEditPart.getChildBySemanticHint(UMLVisualIDRegistry.getType(Package3EditPart.VISUAL_ID));
 		assertNotNull(editPart7);
 		assertEquals("Package", getMetaclassName(editPart7));
+		getDiagramEditPart().getViewer().flush();
+
+	}
+
+	/**
+	 * @generated
+	 */
+	public void testComponentInComponent() {
+		IGraphicalEditPart diagramEditPart = getDiagramEditPart();
+
+		Tool tool2;
+		tool2 = new UnspecifiedTypeCreationTool(Collections.singletonList(ElementTypeRegistry.getInstance().getType("org.eclipse.uml2.diagram.component.Component_2001")));
+
+		createNodeByTool(tool2, 200, 100);
+
+		IGraphicalEditPart editPart8 = diagramEditPart.getChildBySemanticHint(UMLVisualIDRegistry.getType(ComponentEditPart.VISUAL_ID));
+		assertNotNull(editPart8);
+
+		getDiagramEditPart().getViewer().flush();
+
+		IGraphicalEditPart compartment2 = editPart8.getChildBySemanticHint(UMLVisualIDRegistry.getType(ComponentContentsEditPart.VISUAL_ID));
+		assertNotNull(compartment2);
+
+		createNodeByTool(myComponentTool, 201, 150);
+
+		IGraphicalEditPart editPart9 = compartment2.getChildBySemanticHint(UMLVisualIDRegistry.getType(Component2EditPart.VISUAL_ID));
+		assertNotNull(editPart9);
+		assertEquals("Component", getMetaclassName(editPart9));
+		getDiagramEditPart().getViewer().flush();
+
+	}
+
+	/**
+	 * @generated
+	 */
+	public void testComponentInPackage() {
+		IGraphicalEditPart diagramEditPart = getDiagramEditPart();
+
+		Tool tool3;
+		tool3 = new UnspecifiedTypeCreationTool(Collections.singletonList(ElementTypeRegistry.getInstance().getType("org.eclipse.uml2.diagram.component.Package_2006")));
+
+		createNodeByTool(tool3, 200, 100);
+
+		IGraphicalEditPart editPart10 = diagramEditPart.getChildBySemanticHint(UMLVisualIDRegistry.getType(Package3EditPart.VISUAL_ID));
+		assertNotNull(editPart10);
+
+		getDiagramEditPart().getViewer().flush();
+
+		IGraphicalEditPart compartment3 = editPart10.getChildBySemanticHint(UMLVisualIDRegistry.getType(PackageClassifiersEditPart.VISUAL_ID));
+		assertNotNull(compartment3);
+
+		createNodeByTool(myComponentTool, 201, 101);
+
+		IGraphicalEditPart editPart11 = compartment3.getChildBySemanticHint(UMLVisualIDRegistry.getType(Component3EditPart.VISUAL_ID));
+		assertNotNull(editPart11);
+		assertEquals("Component", getMetaclassName(editPart11));
+		getDiagramEditPart().getViewer().flush();
+
+	}
+
+	/**
+	 * @generated
+	 */
+	public void testArtifactInComponent() {
+		IGraphicalEditPart diagramEditPart = getDiagramEditPart();
+
+		Tool tool4;
+		tool4 = new UnspecifiedTypeCreationTool(Collections.singletonList(ElementTypeRegistry.getInstance().getType("org.eclipse.uml2.diagram.component.Component_2001")));
+
+		createNodeByTool(tool4, 200, 100);
+
+		IGraphicalEditPart editPart12 = diagramEditPart.getChildBySemanticHint(UMLVisualIDRegistry.getType(ComponentEditPart.VISUAL_ID));
+		assertNotNull(editPart12);
+
+		getDiagramEditPart().getViewer().flush();
+
+		IGraphicalEditPart compartment4 = editPart12.getChildBySemanticHint(UMLVisualIDRegistry.getType(ComponentContentsEditPart.VISUAL_ID));
+		assertNotNull(compartment4);
+
+		createNodeByTool(myArtifactTool, 201, 150);
+
+		IGraphicalEditPart editPart13 = compartment4.getChildBySemanticHint(UMLVisualIDRegistry.getType(ArtifactEditPart.VISUAL_ID));
+		assertNotNull(editPart13);
+		assertEquals("Artifact", getMetaclassName(editPart13));
+		getDiagramEditPart().getViewer().flush();
+
+	}
+
+	/**
+	 * @generated
+	 */
+	public void testInterfaceInComponent() {
+		IGraphicalEditPart diagramEditPart = getDiagramEditPart();
+
+		Tool tool5;
+		tool5 = new UnspecifiedTypeCreationTool(Collections.singletonList(ElementTypeRegistry.getInstance().getType("org.eclipse.uml2.diagram.component.Component_2001")));
+
+		createNodeByTool(tool5, 200, 100);
+
+		IGraphicalEditPart editPart14 = diagramEditPart.getChildBySemanticHint(UMLVisualIDRegistry.getType(ComponentEditPart.VISUAL_ID));
+		assertNotNull(editPart14);
+
+		getDiagramEditPart().getViewer().flush();
+
+		IGraphicalEditPart compartment5 = editPart14.getChildBySemanticHint(UMLVisualIDRegistry.getType(ComponentContentsEditPart.VISUAL_ID));
+		assertNotNull(compartment5);
+
+		createNodeByTool(myInterfaceTool, 201, 150);
+
+		IGraphicalEditPart editPart15 = compartment5.getChildBySemanticHint(UMLVisualIDRegistry.getType(InterfaceEditPart.VISUAL_ID));
+		assertNotNull(editPart15);
+		assertEquals("Interface", getMetaclassName(editPart15));
+		getDiagramEditPart().getViewer().flush();
+
+	}
+
+	/**
+	 * @generated
+	 */
+	public void testClassInComponent() {
+		IGraphicalEditPart diagramEditPart = getDiagramEditPart();
+
+		Tool tool6;
+		tool6 = new UnspecifiedTypeCreationTool(Collections.singletonList(ElementTypeRegistry.getInstance().getType("org.eclipse.uml2.diagram.component.Component_2001")));
+
+		createNodeByTool(tool6, 200, 100);
+
+		IGraphicalEditPart editPart16 = diagramEditPart.getChildBySemanticHint(UMLVisualIDRegistry.getType(ComponentEditPart.VISUAL_ID));
+		assertNotNull(editPart16);
+
+		getDiagramEditPart().getViewer().flush();
+
+		IGraphicalEditPart compartment6 = editPart16.getChildBySemanticHint(UMLVisualIDRegistry.getType(ComponentContentsEditPart.VISUAL_ID));
+		assertNotNull(compartment6);
+
+		createNodeByTool(myClassTool, 201, 150);
+
+		IGraphicalEditPart editPart17 = compartment6.getChildBySemanticHint(UMLVisualIDRegistry.getType(ClassEditPart.VISUAL_ID));
+		assertNotNull(editPart17);
+		assertEquals("Class", getMetaclassName(editPart17));
+		getDiagramEditPart().getViewer().flush();
+
+	}
+
+	/**
+	 * @generated
+	 */
+	public void testPackageInPackage() {
+		IGraphicalEditPart diagramEditPart = getDiagramEditPart();
+
+		Tool tool7;
+		tool7 = new UnspecifiedTypeCreationTool(Collections.singletonList(ElementTypeRegistry.getInstance().getType("org.eclipse.uml2.diagram.component.Package_2006")));
+
+		createNodeByTool(tool7, 200, 100);
+
+		IGraphicalEditPart editPart18 = diagramEditPart.getChildBySemanticHint(UMLVisualIDRegistry.getType(Package3EditPart.VISUAL_ID));
+		assertNotNull(editPart18);
+
+		getDiagramEditPart().getViewer().flush();
+
+		IGraphicalEditPart compartment7 = editPart18.getChildBySemanticHint(UMLVisualIDRegistry.getType(PackagePackagesEditPart.VISUAL_ID));
+		assertNotNull(compartment7);
+
+		createNodeByTool(myPackageTool, 201, 101);
+
+		IGraphicalEditPart editPart19 = compartment7.getChildBySemanticHint(UMLVisualIDRegistry.getType(Package4EditPart.VISUAL_ID));
+		assertNotNull(editPart19);
+		assertEquals("Package", getMetaclassName(editPart19));
+		getDiagramEditPart().getViewer().flush();
+
+	}
+
+	/**
+	 * @generated
+	 */
+	public void testClassInPackage() {
+		IGraphicalEditPart diagramEditPart = getDiagramEditPart();
+
+		Tool tool8;
+		tool8 = new UnspecifiedTypeCreationTool(Collections.singletonList(ElementTypeRegistry.getInstance().getType("org.eclipse.uml2.diagram.component.Package_2006")));
+
+		createNodeByTool(tool8, 200, 100);
+
+		IGraphicalEditPart editPart20 = diagramEditPart.getChildBySemanticHint(UMLVisualIDRegistry.getType(Package3EditPart.VISUAL_ID));
+		assertNotNull(editPart20);
+
+		getDiagramEditPart().getViewer().flush();
+
+		IGraphicalEditPart compartment8 = editPart20.getChildBySemanticHint(UMLVisualIDRegistry.getType(PackageClassifiersEditPart.VISUAL_ID));
+		assertNotNull(compartment8);
+
+		createNodeByTool(myClassTool, 201, 101);
+
+		IGraphicalEditPart editPart21 = compartment8.getChildBySemanticHint(UMLVisualIDRegistry.getType(Class3EditPart.VISUAL_ID));
+		assertNotNull(editPart21);
+		assertEquals("Class", getMetaclassName(editPart21));
+		getDiagramEditPart().getViewer().flush();
+
+	}
+
+	/**
+	 * @generated
+	 */
+	public void testClassInClass() {
+		IGraphicalEditPart diagramEditPart = getDiagramEditPart();
+
+		Tool tool9;
+		tool9 = new UnspecifiedTypeCreationTool(Collections.singletonList(ElementTypeRegistry.getInstance().getType("org.eclipse.uml2.diagram.component.Class_2007")));
+
+		createNodeByTool(tool9, 200, 100);
+
+		IGraphicalEditPart editPart22 = diagramEditPart.getChildBySemanticHint(UMLVisualIDRegistry.getType(ClassDiagramNotationClassEditPart.VISUAL_ID));
+		assertNotNull(editPart22);
+
+		getDiagramEditPart().getViewer().flush();
+
+		IGraphicalEditPart compartment9 = editPart22.getChildBySemanticHint(UMLVisualIDRegistry.getType(ClassClassesEditPart.VISUAL_ID));
+		assertNotNull(compartment9);
+
+		createNodeByTool(myClassTool, 201, 101);
+
+		IGraphicalEditPart editPart23 = compartment9.getChildBySemanticHint(UMLVisualIDRegistry.getType(ClassDiagramNotationInnerClassEditPart.VISUAL_ID));
+		assertNotNull(editPart23);
+		assertEquals("Class", getMetaclassName(editPart23));
 		getDiagramEditPart().getViewer().flush();
 
 	}
