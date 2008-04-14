fa2c204cf50c372d325117c92cdd27b42966ed79
tfesenko <tfesenko>
2008-04-14 12:10:18 +0000

    Add generated tests for node/children tool groups

--- tests/org.eclipse.uml2.diagram.csd.tests/src/org/eclipse/uml2/diagram/csd/tests/AllTests.java
+++ tests/org.eclipse.uml2.diagram.csd.tests/src/org/eclipse/uml2/diagram/csd/tests/AllTests.java
@@ -3,6 +3,8 @@ package org.eclipse.uml2.diagram.csd.tests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.uml2.diagram.csd.tests.tool.ChildrenGroupTest;
+import org.eclipse.uml2.diagram.csd.tests.tool.NodesGroupTest;
 import org.eclipse.uml2.diagram.csd.whole.TestWholeDiagramSuite;
 
 public class AllTests {
@@ -10,6 +12,8 @@ public class AllTests {
 		TestSuite suite = new TestSuite("Test for org.eclipse.uml2.diagram.csd");
 		//$JUnit-BEGIN$
 		suite.addTest(new TestWholeDiagramSuite());
+		suite.addTestSuite(NodesGroupTest.class);
+		suite.addTestSuite(ChildrenGroupTest.class);
 		//$JUnit-END$
 		return suite;
 	}
