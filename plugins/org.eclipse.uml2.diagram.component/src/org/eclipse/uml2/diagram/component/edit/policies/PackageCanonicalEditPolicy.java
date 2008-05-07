93c1e5c0ab82770dc45d1d8bb735af2b15b129a3
mgolubev <mgolubev>
2008-05-07 18:14:59 +0000

    [230952] - Note (and any other pure notational links) should not be removed by diagram update

--- plugins/org.eclipse.uml2.diagram.component/src/org/eclipse/uml2/diagram/component/edit/policies/PackageCanonicalEditPolicy.java
+++ plugins/org.eclipse.uml2.diagram.component/src/org/eclipse/uml2/diagram/component/edit/policies/PackageCanonicalEditPolicy.java
@@ -204,6 +204,11 @@ public class PackageCanonicalEditPolicy extends CanonicalConnectionEditPolicy {
 				}
 				continue;
 			}
+			//don't remove notation-only links 
+			if (isNotationOnlyEdge(nextDiagramLink)) {
+				linksIterator.remove();
+				continue;
+			}
 			EObject diagramLinkObject = nextDiagramLink.getElement();
 			EObject diagramLinkSrc = nextDiagramLink.getSource().getElement();
 			EObject diagramLinkDst = nextDiagramLink.getTarget().getElement();
@@ -497,6 +502,13 @@ public class PackageCanonicalEditPolicy extends CanonicalConnectionEditPolicy {
 			return (EditPart) getHost().getViewer().getEditPartRegistry().get(view);
 		}
 		return null;
+	}
+
+	/**
+	 * @generated
+	 */
+	private boolean isNotationOnlyEdge(Edge edge) {
+		return false;
 	}
 
 	/**
