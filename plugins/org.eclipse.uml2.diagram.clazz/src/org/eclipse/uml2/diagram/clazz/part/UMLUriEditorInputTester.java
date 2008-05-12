4a8a5cf5ad5186ff0a84ea0199b69ab721bd6d93
mgolubev <mgolubev>
2008-05-12 11:18:31 +0000

    [230873] - Make diagram files extension shorter

--- plugins/org.eclipse.uml2.diagram.clazz/src/org/eclipse/uml2/diagram/clazz/part/UMLUriEditorInputTester.java
+++ plugins/org.eclipse.uml2.diagram.clazz/src/org/eclipse/uml2/diagram/clazz/part/UMLUriEditorInputTester.java
@@ -16,7 +16,9 @@ public class UMLUriEditorInputTester extends PropertyTester {
 			return false;
 		}
 		URIEditorInput editorInput = (URIEditorInput) receiver;
-		return "umlclass_diagram".equals(editorInput.getURI().fileExtension()); //$NON-NLS-1$
+		return "umlclass".equals(editorInput.getURI().fileExtension())//$NON-NLS-1$
+				|| "umlclass_diagram".equals(editorInput.getURI().fileExtension())//$NON-NLS-1$
+		;
 	}
 
 }
