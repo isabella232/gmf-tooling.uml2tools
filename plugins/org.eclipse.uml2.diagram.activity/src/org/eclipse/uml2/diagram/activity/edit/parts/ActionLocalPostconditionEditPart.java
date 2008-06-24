33ca52ca156321ba50b5f6a0042b08223adc5cf4
mgolubev <mgolubev>
2008-06-24 10:22:52 +0000

    Regenerated with GMF R2.1.0, with regards to [206348] - tree branch option is true by default

--- plugins/org.eclipse.uml2.diagram.activity/src/org/eclipse/uml2/diagram/activity/edit/parts/ActionLocalPostconditionEditPart.java
+++ plugins/org.eclipse.uml2.diagram.activity/src/org/eclipse/uml2/diagram/activity/edit/parts/ActionLocalPostconditionEditPart.java
@@ -3,6 +3,7 @@ package org.eclipse.uml2.diagram.activity.edit.parts;
 import org.eclipse.draw2d.Connection;
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ConnectionNodeEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ITreeBranchEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.PolylineConnectionEx;
 import org.eclipse.gmf.runtime.notation.View;
@@ -11,7 +12,7 @@ import org.eclipse.uml2.diagram.activity.edit.policies.ActionLocalPostconditionI
 /**
  * @generated
  */
-public class ActionLocalPostconditionEditPart extends ConnectionNodeEditPart {
+public class ActionLocalPostconditionEditPart extends ConnectionNodeEditPart implements ITreeBranchEditPart {
 
 	/**
 	 * @generated
--- plugins/org.eclipse.uml2.diagram.activity/src/org/eclipse/uml2/diagram/activity/edit/parts/ObjectFlowEditPart.java
+++ plugins/org.eclipse.uml2.diagram.activity/src/org/eclipse/uml2/diagram/activity/edit/parts/ObjectFlowEditPart.java
@@ -7,6 +7,7 @@ import org.eclipse.draw2d.RotatableDecoration;
 import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ConnectionNodeEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ITreeBranchEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.PolylineConnectionEx;
 import org.eclipse.gmf.runtime.notation.View;
@@ -15,7 +16,7 @@ import org.eclipse.uml2.diagram.activity.edit.policies.ObjectFlowItemSemanticEdi
 /**
  * @generated
  */
-public class ObjectFlowEditPart extends ConnectionNodeEditPart {
+public class ObjectFlowEditPart extends ConnectionNodeEditPart implements ITreeBranchEditPart {
 
 	/**
 	 * @generated
