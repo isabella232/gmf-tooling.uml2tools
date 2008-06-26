c7e683000a07ca95471f31e432d4c9aab447c41f
mgolubev <mgolubev>
2008-06-26 13:40:45 +0000

    #235173 '[ComponentD] Support for Associations' - set blue color to association Super class AssociationLinkFigureBase was refactored into common plugin

--- plugins/org.eclipse.uml2.diagram.clazz/src/org/eclipse/uml2/diagram/clazz/edit/parts/AssociationEditPart.java
+++ plugins/org.eclipse.uml2.diagram.clazz/src/org/eclipse/uml2/diagram/clazz/edit/parts/AssociationEditPart.java
@@ -4,18 +4,16 @@ import java.util.Iterator;
 
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Connection;
-import org.eclipse.draw2d.RotatableDecoration;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ConnectionNodeEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ITreeBranchEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
-import org.eclipse.gmf.runtime.draw2d.ui.figures.PolylineConnectionEx;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.uml2.diagram.clazz.edit.policies.AssociationItemSemanticEditPolicy;
 import org.eclipse.uml2.diagram.common.conventions.AssociationEndConvention;
-import org.eclipse.uml2.diagram.common.draw2d.decoration.AssociationDecoration;
+import org.eclipse.uml2.diagram.common.draw2d.AssociationLinkFigureBase;
 import org.eclipse.uml2.uml.Association;
 import org.eclipse.uml2.uml.Property;
 
@@ -131,48 +129,33 @@ public class AssociationEditPart extends ConnectionNodeEditPart implements ITree
 	/**
 	 * @generated
 	 */
-	public class AssociationLinkFigure extends PolylineConnectionEx {
+	public class AssociationLinkFigure extends AssociationLinkFigureBase {
 
 		/**
 		 * @generated
 		 */
 		public AssociationLinkFigure() {
+
 			this.setForegroundColor(ColorConstants.blue);
-
-			setSourceDecoration(createSourceDecoration());
-			setTargetDecoration(createTargetDecoration());
 		}
 
 		/**
 		 * @generated
 		 */
-		private RotatableDecoration createSourceDecoration() {
-			AssociationDecoration df = new AssociationDecoration();
+		private boolean myUseLocalCoordinates = false;
 
-			return df;
+		/**
+		 * @generated
+		 */
+		protected boolean useLocalCoordinates() {
+			return myUseLocalCoordinates;
 		}
 
 		/**
 		 * @generated
 		 */
-		private RotatableDecoration createTargetDecoration() {
-			AssociationDecoration df = new AssociationDecoration();
-
-			return df;
-		}
-
-		/**
-		 * @generated NOT
-		 */
-		private AssociationDecoration getTargetDecorationImpl() {
-			return (AssociationDecoration) getTargetDecoration();
-		}
-
-		/**
-		 * @generated NOT
-		 */
-		private AssociationDecoration getSourceDecorationImpl() {
-			return (AssociationDecoration) getSourceDecoration();
+		protected void setUseLocalCoordinates(boolean useLocalCoordinates) {
+			myUseLocalCoordinates = useLocalCoordinates;
 		}
 
 	}
