f2a56a3c6cb0d105c77dddb13de8d6c3b2e7d9cc
tfesenko <tfesenko>
2008-09-04 09:40:55 +0000

    [246081]  Stereotype properties in the property editor

--- plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/sheet/PropertySourceExtension.java
+++ plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/sheet/PropertySourceExtension.java
@@ -1,22 +1,15 @@
 package org.eclipse.uml2.diagram.common.sheet;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.gmf.runtime.emf.ui.properties.descriptors.EMFCompositePropertySource;
 import org.eclipse.ui.views.properties.IPropertyDescriptor;
-import org.eclipse.uml2.uml.edit.providers.ElementItemProvider;
 
 public class PropertySourceExtension extends EMFCompositePropertySource {
 
 	private final AdapterFactory myItemProvidersAdapterFactory;
-
-	private List<IItemPropertyDescriptor> stereotypeApplicationItemPropertyDescriptors = null;
 
 	protected PropertySourceExtension(Object object, IItemPropertySource itemPropertySource, String category, AdapterFactory itemProvidersAdapterFactory) {
 		super(object, itemPropertySource, category);
@@ -26,26 +19,6 @@ public class PropertySourceExtension extends EMFCompositePropertySource {
 	public PropertySourceExtension(Object object, IItemPropertySource itemPropertySource, AdapterFactory itemProvidersAdapterFactory) {
 		super(object, itemPropertySource);
 		myItemProvidersAdapterFactory = itemProvidersAdapterFactory;
-	}
-
-	@Override
-	public IPropertyDescriptor[] getPropertyDescriptors() {
-		List<IPropertyDescriptor> propertyDescriptors = new ArrayList<IPropertyDescriptor>();
-		propertyDescriptors.addAll(Arrays.asList(super.getPropertyDescriptors()));
-
-		if (itemPropertySource instanceof ElementItemProvider) {
-			ElementItemProvider elementItemProvider = (ElementItemProvider) itemPropertySource;
-			stereotypeApplicationItemPropertyDescriptors = elementItemProvider.getStereotypeApplicationPropertyDescriptors(object);
-
-			if (stereotypeApplicationItemPropertyDescriptors != null) {
-
-				for (IItemPropertyDescriptor itemPropertyDescriptor : stereotypeApplicationItemPropertyDescriptors) {
-					propertyDescriptors.add(createPropertyDescriptor(itemPropertyDescriptor));
-				}
-			}
-		}
-
-		return propertyDescriptors.toArray(new IPropertyDescriptor[propertyDescriptors.size()]);
 	}
 
 	@Override
