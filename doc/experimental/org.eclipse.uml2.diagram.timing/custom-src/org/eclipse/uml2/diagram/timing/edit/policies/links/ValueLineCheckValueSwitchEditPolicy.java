4dafbffb3c1aa34ca7a0980603e77e2e4e4f5a45
mgolubev <mgolubev>
2008-09-09 08:29:48 +0000

    [242709] - Don't extends SematicEditPolicy -- otherwice edithelpers are asked twice for each editRequestWrapper

--- doc/experimental/org.eclipse.uml2.diagram.timing/custom-src/org/eclipse/uml2/diagram/timing/edit/policies/links/ValueLineCheckValueSwitchEditPolicy.java
+++ doc/experimental/org.eclipse.uml2.diagram.timing/custom-src/org/eclipse/uml2/diagram/timing/edit/policies/links/ValueLineCheckValueSwitchEditPolicy.java
@@ -1,15 +1,32 @@
 package org.eclipse.uml2.diagram.timing.edit.policies.links;
 
+import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.editpolicies.AbstractEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
+import org.eclipse.gmf.runtime.diagram.ui.requests.EditCommandRequestWrapper;
+import org.eclipse.gmf.runtime.diagram.ui.requests.RequestConstants;
 import org.eclipse.gmf.runtime.emf.type.core.requests.CreateRelationshipRequest;
-import org.eclipse.uml2.diagram.timing.edit.policies.TimingDBaseItemSemanticEditPolicy;
+import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
 import org.eclipse.uml2.diagram.timing.providers.TimingDElementTypes;
 
-public class ValueLineCheckValueSwitchEditPolicy extends TimingDBaseItemSemanticEditPolicy {
+public class ValueLineCheckValueSwitchEditPolicy extends AbstractEditPolicy {
 	public static final String ROLE = ValueLineCheckValueSwitchEditPolicy.class.getName() + ":Role";
 	
-	public ValueLineCheckValueSwitchEditPolicy() {
-		super(TimingDElementTypes.DValueLine_3002);
+	@Override
+	public Command getCommand(Request request) {
+		if (RequestConstants.REQ_SEMANTIC_WRAPPER.equals(request.getType())) {
+			return getSemanticCommand(((EditCommandRequestWrapper) request).getEditCommandRequest());
+		}
+		return null;
+	}
+	
+	protected Command getSemanticCommand(IEditCommandRequest req) {
+		//XXX: this editpolicy provides answers to wrappered CreateRelationshipRequest's only
+		if (req instanceof CreateRelationshipRequest) {
+			return getCreateRelationshipCommand((CreateRelationshipRequest) req);
+		}
+		return null;
 	}
 
 	protected Command getCreateRelationshipCommand(CreateRelationshipRequest req) {
@@ -18,7 +35,7 @@ public class ValueLineCheckValueSwitchEditPolicy extends TimingDBaseItemSemantic
 
 	protected Command getCompleteCreateRelationshipCommand(CreateRelationshipRequest req) {
 		if (TimingDElementTypes.DStateSwitch_4001 == req.getElementType()) {
-			return getGEFWrapper(new SegmentCheckValueSwitchEditPolicy.CheckValueSwitchCommand(req, req.getSource(), req.getTarget()));
+			return new ICommandProxy(new SegmentCheckValueSwitchEditPolicy.CheckValueSwitchCommand(req, req.getSource(), req.getTarget()));
 		}
 		return null;
 	}
