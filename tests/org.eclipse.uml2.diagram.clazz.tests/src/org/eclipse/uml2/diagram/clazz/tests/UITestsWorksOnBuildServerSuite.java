75869fba2228a30626e1b4a974edd62594de6fbc
mgolubev <mgolubev>
2008-10-07 11:22:08 +0000

    [248969] - Adding simple test that UIApplication can be started on build server

---
+++ tests/org.eclipse.uml2.diagram.clazz.tests/src/org/eclipse/uml2/diagram/clazz/tests/UITestsWorksOnBuildServerSuite.java
@@ -0,0 +1,24 @@
+package org.eclipse.uml2.diagram.clazz.tests;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.eclipse.ui.PlatformUI;
+
+
+public class UITestsWorksOnBuildServerSuite {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Checking that UI tests can be run on build server");
+		suite.addTest(new SimpleUITest());
+		return suite;
+	}
+	
+	public static class SimpleUITest extends TestCase {
+		public void testWorkbenchStarted(){
+			assertNotNull(PlatformUI.getWorkbench());
+			assertTrue(PlatformUI.isWorkbenchRunning());
+		}
+	}
+}
