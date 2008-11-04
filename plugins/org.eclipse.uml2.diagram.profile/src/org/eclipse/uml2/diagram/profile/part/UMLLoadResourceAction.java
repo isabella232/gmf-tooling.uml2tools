c48495b2d2edacc48a82aa30ec608024380daafe
tfesenko <tfesenko>
2008-11-04 16:42:55 +0000

    [246361] Shortcuts on ProfileDefinition diagram

--- plugins/org.eclipse.uml2.diagram.profile/src/org/eclipse/uml2/diagram/profile/part/UMLLoadResourceAction.java
+++ plugins/org.eclipse.uml2.diagram.profile/src/org/eclipse/uml2/diagram/profile/part/UMLLoadResourceAction.java
@@ -1,64 +1,38 @@
 package org.eclipse.uml2.diagram.profile.part;
 
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.emf.edit.ui.action.LoadResourceAction;
 import org.eclipse.emf.edit.ui.action.LoadResourceAction.LoadResourceDialog;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.uml2.diagram.profile.edit.parts.ProfileEditPart;
 
 /**
  * @generated
  */
-public class UMLLoadResourceAction implements IObjectActionDelegate {
+public class UMLLoadResourceAction extends AbstractHandler {
 
 	/**
 	 * @generated
 	 */
-	private ProfileEditPart mySelectedElement;
-
-	/**
-	 * @generated
-	 */
-	private Shell myShell;
-
-	/**
-	 * @generated
-	 */
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		myShell = targetPart.getSite().getShell();
-	}
-
-	/**
-	 * @generated
-	 */
-	public void run(IAction action) {
-		LoadResourceAction.LoadResourceDialog loadResourceDialog = new LoadResourceAction.LoadResourceDialog(myShell, mySelectedElement.getEditingDomain());
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IEditorPart diagramEditor = HandlerUtil.getActiveEditorChecked(event);
+		Shell shell = diagramEditor.getEditorSite().getShell();
+		assert diagramEditor instanceof DiagramEditor;
+		TransactionalEditingDomain editingDomain = ((DiagramEditor) diagramEditor).getEditingDomain();
+		LoadResourceAction.LoadResourceDialog loadResourceDialog = new LoadResourceAction.LoadResourceDialog(shell, editingDomain);
 		loadResourceDialog.open();
-	}
-
-	/**
-	 * @generated
-	 */
-	public void selectionChanged(IAction action, ISelection selection) {
-		mySelectedElement = null;
-		if (selection instanceof IStructuredSelection) {
-			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
-			if (structuredSelection.size() == 1 && structuredSelection.getFirstElement() instanceof ProfileEditPart) {
-				mySelectedElement = (ProfileEditPart) structuredSelection.getFirstElement();
-			}
-		}
-		action.setEnabled(isEnabled());
-	}
-
-	/**
-	 * @generated
-	 */
-	private boolean isEnabled() {
-		return mySelectedElement != null;
+		return null;
 	}
 
 }
