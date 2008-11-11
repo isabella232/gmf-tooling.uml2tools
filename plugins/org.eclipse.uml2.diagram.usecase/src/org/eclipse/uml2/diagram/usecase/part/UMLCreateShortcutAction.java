8ce3c45f9e0e0376c01c8bead021d8705a66eff9
mgolubev <mgolubev>
2008-11-11 16:52:10 +0000

    [254898] - Add shortcut action should be added explicitly [247542] - generate only actually used preference pages

--- plugins/org.eclipse.uml2.diagram.usecase/src/org/eclipse/uml2/diagram/usecase/part/UMLCreateShortcutAction.java
+++ plugins/org.eclipse.uml2.diagram.usecase/src/org/eclipse/uml2/diagram/usecase/part/UMLCreateShortcutAction.java
@@ -1,102 +1,76 @@
 package org.eclipse.uml2.diagram.usecase.part;
 
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.operations.OperationHistoryFactory;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.common.util.WrappedException;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gef.EditPart;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
 import org.eclipse.gmf.runtime.diagram.ui.commands.CreateCommand;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
 import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequest;
 import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
 import org.eclipse.gmf.runtime.notation.Node;
 import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.uml2.diagram.usecase.edit.commands.UMLCreateShortcutDecorationsCommand;
-import org.eclipse.uml2.diagram.usecase.edit.parts.PackageEditPart;
 
 /**
  * @generated
  */
 
-public class UMLCreateShortcutAction implements IObjectActionDelegate {
+public class UMLCreateShortcutAction extends AbstractHandler {
 
 	/**
 	 * @generated
 	 */
-	private PackageEditPart mySelectedElement;
-
-	/**
-	 * @generated
-	 */
-	private Shell myShell;
-
-	/**
-	 * @generated
-	 */
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		myShell = targetPart.getSite().getShell();
-	}
-
-	/**
-	 * @generated
-	 */
-	public void selectionChanged(IAction action, ISelection selection) {
-		mySelectedElement = null;
-		if (selection instanceof IStructuredSelection) {
-			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
-			if (structuredSelection.size() == 1 && structuredSelection.getFirstElement() instanceof PackageEditPart) {
-				mySelectedElement = (PackageEditPart) structuredSelection.getFirstElement();
-			}
-		}
-		action.setEnabled(isEnabled());
-	}
-
-	/**
-	 * @generated
-	 */
-	private boolean isEnabled() {
-		return mySelectedElement != null;
-	}
-
-	/**
-	 * @generated
-	 */
-	public void run(IAction action) {
-		final View view = (View) mySelectedElement.getModel();
-		UMLElementChooserDialog elementChooser = new UMLElementChooserDialog(myShell, view);
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IEditorPart diagramEditor = HandlerUtil.getActiveEditorChecked(event);
+		Shell shell = diagramEditor.getEditorSite().getShell();
+		assert diagramEditor instanceof DiagramEditor;
+		TransactionalEditingDomain editingDomain = ((DiagramEditor) diagramEditor).getEditingDomain();
+		ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
+		assert selection instanceof IStructuredSelection;
+		assert ((IStructuredSelection) selection).size() == 1;
+		assert ((IStructuredSelection) selection).getFirstElement() instanceof EditPart;
+		EditPart selectedDiagramPart = (EditPart) ((IStructuredSelection) selection).getFirstElement();
+		final View view = (View) selectedDiagramPart.getModel();
+		UMLElementChooserDialog elementChooser = new UMLElementChooserDialog(shell, view);
 		int result = elementChooser.open();
 		if (result != Window.OK) {
-			return;
+			return null;
 		}
 		URI selectedModelElementURI = elementChooser.getSelectedModelElementURI();
 		final EObject selectedElement;
 		try {
-			selectedElement = mySelectedElement.getEditingDomain().getResourceSet().getEObject(selectedModelElementURI, true);
+			selectedElement = editingDomain.getResourceSet().getEObject(selectedModelElementURI, true);
 		} catch (WrappedException e) {
 			UMLDiagramEditorPlugin.getInstance().logError("Exception while loading object: " + selectedModelElementURI.toString(), e); //$NON-NLS-1$
-			return;
+			return null;
 		}
 
 		if (selectedElement == null) {
-			return;
+			return null;
 		}
 		CreateViewRequest.ViewDescriptor viewDescriptor = new CreateViewRequest.ViewDescriptor(new EObjectAdapter(selectedElement), Node.class, null, UMLDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT);
-		ICommand command = new CreateCommand(mySelectedElement.getEditingDomain(), viewDescriptor, view);
-		command = command.compose(new UMLCreateShortcutDecorationsCommand(mySelectedElement.getEditingDomain(), view, viewDescriptor));
+		ICommand command = new CreateCommand(editingDomain, viewDescriptor, view);
+		command = command.compose(new UMLCreateShortcutDecorationsCommand(editingDomain, view, viewDescriptor));
 		try {
 			OperationHistoryFactory.getOperationHistory().execute(command, new NullProgressMonitor(), null);
-			UMLDiagramUpdateCommand.performCanonicalUpdate(view.getDiagram().getElement());
 		} catch (ExecutionException e) {
 			UMLDiagramEditorPlugin.getInstance().logError("Unable to create shortcut", e); //$NON-NLS-1$
 		}
+		return null;
 	}
 
 }
