f19c4493e88705df9433d073dee72a8d7ef10fa9
tfesenko <tfesenko>
2009-01-08 13:29:09 +0000

    [253939] Allow to stereotype a diagram

---
+++ plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/parser/stereotype/PackageAppliedStereotypeParser.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2006, 2008 Borland Software Corporation
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Tatiana Fesenko (Borland) - initial API and implementation
+ */
+package org.eclipse.uml2.diagram.common.parser.stereotype;
+
+import org.eclipse.uml2.uml.Element;
+
+public class PackageAppliedStereotypeParser extends ClassifierAppliedStereotypeParser {
+	private static final String PACKAGE_LABEL = "package"; //$NON-NLS-1$
+
+	@Override
+	protected String getElementLabel(Element element) {
+		return PACKAGE_LABEL;
+	}
+
+}
