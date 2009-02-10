06568d4b783a8a92e89c327eb79204436791ffb4
mgolubev <mgolubev>
2009-02-10 19:26:29 +0000

    [259490] - Constraints/Comments figures fixed with GEF M5

--- plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/draw2d/MultilineConstraintFigure.java
+++ plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/draw2d/MultilineConstraintFigure.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006 Borland Software Corporation
+ * Copyright (c) 2006, 2009 Borland Software Corporation
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,15 +11,8 @@
  */
 package org.eclipse.uml2.diagram.common.draw2d;
 
-import org.eclipse.draw2d.MarginBorder;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.WrappingLabel;
 
-/**
- * XXX: It is temporary hack.
- * 
- * To be removed when GMF graph will support multiline labels. If it is not
- * achiveable, move this into the generation using custom viewmap attributes.
- */
 public class MultilineConstraintFigure extends ConstraintFigureBase {
 
 	private WrappingLabel myLanguage;
@@ -28,7 +21,7 @@ public class MultilineConstraintFigure extends ConstraintFigureBase {
 
 	public MultilineConstraintFigure() {
 		super();
-		setBorder(new MarginBorder(MARGIN_DP, MARGIN_DP, MARGIN_DP, CLIP_MARGIN_DP));
+
 		myLanguage = new WrappingLabel();
 		add(myLanguage);
 		myBody = new WrappingLabel();
