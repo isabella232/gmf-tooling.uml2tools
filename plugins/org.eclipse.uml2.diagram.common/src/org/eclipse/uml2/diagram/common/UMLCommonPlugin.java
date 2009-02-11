e1590bb008d78c8d50edbfade71e156150311186
mgolubev <mgolubev>
2009-02-11 16:38:19 +0000

    [261356] - copyright, etc, no structural changes

--- plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/UMLCommonPlugin.java
+++ plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/UMLCommonPlugin.java
@@ -1,15 +1,18 @@
-/**
+/*
+ * Copyright (c) 2009 Borland Software Corporation
  * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Tatiana Fesenko (Borland) - initial API and implementation
  */
 package org.eclipse.uml2.diagram.common;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
-
-/**
- * @author tfesenko
- *
- */
 public class UMLCommonPlugin extends AbstractUIPlugin {
 
 	private static UMLCommonPlugin plugin;
