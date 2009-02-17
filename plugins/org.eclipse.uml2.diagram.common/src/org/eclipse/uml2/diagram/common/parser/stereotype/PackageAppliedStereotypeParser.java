6ba3edb14d7f7f74de283d2d7625be7d9276aabc
tfesenko <tfesenko>
2009-02-17 16:52:11 +0000

    [265172] Place keyword «model» above the name of the Model

--- plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/parser/stereotype/PackageAppliedStereotypeParser.java
+++ plugins/org.eclipse.uml2.diagram.common/src/org/eclipse/uml2/diagram/common/parser/stereotype/PackageAppliedStereotypeParser.java
@@ -12,12 +12,18 @@
 package org.eclipse.uml2.diagram.common.parser.stereotype;
 
 import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Model;
 
 public class PackageAppliedStereotypeParser extends ClassifierAppliedStereotypeParser {
+
 	private static final String PACKAGE_LABEL = "package"; //$NON-NLS-1$
 
+	private static final String MODEL_LABEL = "model"; //$NON-NLS-1$
 	@Override
 	protected String getElementLabel(Element element) {
+		if (element instanceof Model) {
+			return MODEL_LABEL;
+		}
 		return PACKAGE_LABEL;
 	}
 
