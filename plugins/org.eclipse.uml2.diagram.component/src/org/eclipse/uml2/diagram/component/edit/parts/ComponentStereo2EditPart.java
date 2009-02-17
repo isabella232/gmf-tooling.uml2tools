8463192d6ef8bf496aa1063c8850a15379396717
mgolubev <mgolubev>
2009-02-17 20:12:44 +0000

    [265026] - All Diagrams - If node has both stereotype and name labels, there should be a way to activate them separately - regenerated for ComponentD/Component

--- plugins/org.eclipse.uml2.diagram.component/src/org/eclipse/uml2/diagram/component/edit/parts/ComponentStereo2EditPart.java
+++ plugins/org.eclipse.uml2.diagram.component/src/org/eclipse/uml2/diagram/component/edit/parts/ComponentStereo2EditPart.java
@@ -352,7 +352,12 @@ public class ComponentStereo2EditPart extends CompartmentEditPart implements ITe
 	 * @generated
 	 */
 	private void performDirectEdit(char initialCharacter) {
-		if (getManager() instanceof TextDirectEditManager) {
+		// '<' has special meaning, because we have both name- and stereo- inplaces for single node edit part
+		// we want to actiavate stereotype inplace if user presses '<' (for "<< stereotype >>" 
+		// notation, also we don't include '<' and '>' into actual inplace text).
+		// If user presses any other alfanum key, we will activate name-inplace, as for all other figures
+
+		if (initialCharacter != '<' && getManager() instanceof TextDirectEditManager) {
 			((TextDirectEditManager) getManager()).show(initialCharacter);
 		} else {
 			performDirectEdit();
