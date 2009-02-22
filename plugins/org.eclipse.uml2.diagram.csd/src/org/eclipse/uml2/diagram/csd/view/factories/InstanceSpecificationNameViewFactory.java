7efeafaaf73fa9e72a026fed72410ad26b3f4e22
mgolubev <mgolubev>
2009-02-22 16:08:10 +0000

    [263848]  Improve appearance of Value Specification - reformatted on regeneration, no structural changes

--- plugins/org.eclipse.uml2.diagram.csd/src/org/eclipse/uml2/diagram/csd/view/factories/InstanceSpecificationNameViewFactory.java
+++ plugins/org.eclipse.uml2.diagram.csd/src/org/eclipse/uml2/diagram/csd/view/factories/InstanceSpecificationNameViewFactory.java
@@ -31,7 +31,7 @@ public class InstanceSpecificationNameViewFactory extends BasicNodeViewFactory {
 		styles.add(NotationFactory.eINSTANCE.createFontStyle());
 		return styles;
 	}
-	
+
 	/**
 	 * @NOT-generated
 	 */
