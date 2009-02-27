ec5089b6b04206ca40a3f67a2f628c2f7c47b9bf
mgolubev <mgolubev>
2009-02-27 18:26:26 +0000

    [230963] - SeqD - debug - Debug info added to AnchoredSibling

--- plugins/experimental/org.eclipse.uml2.diagram.sequence/custom-src/org/eclipse/uml2/diagram/sequence/edit/policies/OrderedLayoutEditPolicy.java
+++ plugins/experimental/org.eclipse.uml2.diagram.sequence/custom-src/org/eclipse/uml2/diagram/sequence/edit/policies/OrderedLayoutEditPolicy.java
@@ -1,12 +1,13 @@
 package org.eclipse.uml2.diagram.sequence.edit.policies;
 
 import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.notation.View;
 
 public interface OrderedLayoutEditPolicy {
 	public AnchoredSibling findAnchoredSibling(Point relativeLocation);
 	
-	public static class AnchoredSibling {
+	public final static class AnchoredSibling {
 		private final View mySiblingView;
 		private final boolean myBeforeNotAfterAnchor;
 		
@@ -22,5 +23,22 @@ public interface OrderedLayoutEditPolicy {
 		public boolean isBeforeNotAfterAnchor(){
 			return myBeforeNotAfterAnchor;
 		}	
+		
+		@Override
+		public String toString() {
+			StringBuilder result = new StringBuilder("[AnchoredSibling]: ");
+			if (isBeforeNotAfterAnchor()){
+				result.append("before: ");
+			} else {
+				result.append("after: ");
+			}
+			if (getSiblingView() != null){
+				EObject entity = getSiblingView().getElement();
+				result.append(String.valueOf(entity));
+				result.append(", ");
+			} 
+			result.append("View: ").append(getSiblingView());
+			return result.toString();
+		}
 	}
 }
