49225c5b3b4bed1a68fbfb4be1ec8bf7d6db86a8
mgolubev <mgolubev>
2009-03-19 22:45:04 +0000

    [267049] - Tie frame (general anchor search implemented)

---
+++ plugins/experimental/org.eclipse.uml2.diagram.sequence.model/custom-src/org/eclipse/uml2/diagram/sequence/model/sequenced/PasteDestination.java
@@ -0,0 +1,36 @@
+package org.eclipse.uml2.diagram.sequence.model.sequenced;
+
+public class PasteDestination {
+	private SDEntity myContainer;
+	private SDEntity myAnchor;
+	private boolean myIsAfter;
+	
+	public PasteDestination(SDEntity container){
+		this(container, null, false);
+	}
+	
+	public PasteDestination(SDEntity container, SDEntity anchor, boolean isAfter){
+		if (container == null){
+			throw new IllegalArgumentException("Container is null");
+		}
+		myContainer = container;
+		myAnchor = anchor;
+		myIsAfter = isAfter;
+	}
+	
+	public SDEntity getContainer(){
+		return myContainer;
+	}
+	
+	public SDEntity getAnchor(){
+		return myAnchor;
+	}
+	
+	public boolean isAfterAnchor(){
+		return myIsAfter;
+	}
+	
+	public boolean isFirstElement(){
+		return !myIsAfter && myAnchor == null; 
+	}
+}
