a76c00770d3dd89d2e779c89ef6086c75316d468
mgolubev <mgolubev>
2009-03-25 14:44:27 +0000

    [267049] - Tie frame - refactoring - theOnlyChildFor() private helper is obsolete +reformatted

--- plugins/experimental/org.eclipse.uml2.diagram.sequence.model/custom-src/org/eclipse/uml2/diagram/sequence/model/edit/SDAnchor.java
+++ plugins/experimental/org.eclipse.uml2.diagram.sequence.model/custom-src/org/eclipse/uml2/diagram/sequence/model/edit/SDAnchor.java
@@ -5,56 +5,51 @@ import java.util.List;
 import org.eclipse.uml2.diagram.sequence.model.sequenced.SDBracket;
 import org.eclipse.uml2.diagram.sequence.model.sequenced.SDBracketContainer;
 
-
 public class SDAnchor {
+
 	private final SDBracketContainer myContainer;
+
 	private final SDBracket myAnchor;
+
 	private final boolean myIsBeforeNotAfterAnchor;
 
-	public SDAnchor(SDBracketContainer container, SDBracket anchor, boolean isBeforeNotAfterAnchor){
+	public SDAnchor(SDBracketContainer container, SDBracket anchor, boolean isBeforeNotAfterAnchor) {
 		myContainer = container;
 		myAnchor = anchor;
 		myIsBeforeNotAfterAnchor = isBeforeNotAfterAnchor;
 	}
-	
+
 	public boolean isBeforeNotAfterAnchor() {
 		return myIsBeforeNotAfterAnchor;
 	}
-	
-	public boolean isAfterAnchor(){
+
+	public boolean isAfterAnchor() {
 		return !myIsBeforeNotAfterAnchor;
 	}
-	
-	public boolean isFirstElement(){
-		return myIsBeforeNotAfterAnchor && myAnchor == null; 
+
+	public boolean isFirstElement() {
+		return myIsBeforeNotAfterAnchor && myAnchor == null;
 	}
-	
+
 	public SDBracket getAnchor() {
 		return myAnchor;
 	}
-	
-	public SDBracketContainer getContainer(){
+
+	public SDBracketContainer getContainer() {
 		return myContainer;
 	}
-	
-	public static SDAnchor after(SDBracket bracket){
+
+	public static SDAnchor after(SDBracket bracket) {
 		return new SDAnchor(bracket.getBracketContainer(), bracket, false);
 	}
-	
-	public static SDAnchor firstChildFor(SDBracketContainer container){
+
+	public static SDAnchor firstChildFor(SDBracketContainer container) {
 		return new SDAnchor(container, null, true);
 	}
-	
-	public static SDAnchor lastChildFor(SDBracketContainer container){
-		List<SDBracket> brackets = container.getBrackets(); 
-		return brackets.isEmpty() ? theOnlyChildFor(container) : after(brackets.get(brackets.size() - 1));
-	}
-	
-	private static SDAnchor theOnlyChildFor(SDBracketContainer container){
-		if (!container.getBrackets().isEmpty()){
-			throw new IllegalArgumentException("Container already has brackets " + container);
-		}
-		return new SDAnchor(container, null, true);
+
+	public static SDAnchor lastChildFor(SDBracketContainer container) {
+		List<SDBracket> brackets = container.getBrackets();
+		return brackets.isEmpty() ? firstChildFor(container) : after(brackets.get(brackets.size() - 1));
 	}
 	
 }
